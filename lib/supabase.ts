be4789ebee97fe948c0a3817e33933a5d2f5b652
kyooo8 <kyosuke.7876@gmail.com>
2024-09-29 18:54:13 +0900

    update

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,5 @@
 // src/lib/supabase.ts
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
-import "$std/dotenv/load.ts";
 
 const supabaseUrl = Deno.env.get("SUPABASE_URL");
 const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY");
