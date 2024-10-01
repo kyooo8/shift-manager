22d975858fbde3228231f2703afcc3c9fb4bcc48
kyooo8 <kyosuke.7876@gmail.com>
2024-10-01 20:47:25 +0900

    s

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,10 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
-const supabaseUrl = Deno.env.get("SUPABASE_URL");
-const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY");
+// const supabaseUrl = Deno.env.get("SUPABASE_URL");
+const supabaseUrl = "https://mfzcsojkjhbncuixqffq.supabase.co";
+// const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY");
+const supabaseAnonKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1memNzb2pramhibmN1aXhxZmZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUyODc1NTAsImV4cCI6MjA0MDg2MzU1MH0.yViyJUvTwNOOkDzuG-AaEGKCuWpJUFtErgwe-8yaiBQ";
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("Supabase URL or anon key is missing");
