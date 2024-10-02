8d4730414313c76397b949af4aa4c043d6cba2d3
kyooo8 <kyosuke.7876@gmail.com>
2024-10-03 01:56:11 +0900

    update

--- dev.ts
+++ dev.ts
@@ -1,4 +1,8 @@
-import { start } from "$fresh/server.ts";
-import manifest from "./fresh.gen.ts";
+#!/usr/bin/env -S deno run -A --watch=static/,routes/
 
-await start(manifest);
+import dev from "$fresh/dev.ts";
+import config from "./fresh.config.ts";
+
+import "$std/dotenv/load.ts";
+
+await dev(import.meta.url, "./main.ts", config);
--- routes/api/login.ts
+++ routes/api/login.ts
@@ -1,7 +1,7 @@
-//routes/api/login.ts
 export function handler(req: Request): Response {
   const clientId = Deno.env.get("GOOGLE_CLIENT_ID")!;
-  const redirectUri = "http://localhost:8000/api/callback"; // 認証後のリダイレクト先
+  const redirectUri = Deno.env.get("REDIRECT_URI") ||
+    "http://localhost:8000/api/callback"; // 環境変数から取得
   const scope = [
     "https://www.googleapis.com/auth/calendar",
     "https://www.googleapis.com/auth/userinfo.profile",
@@ -11,6 +11,5 @@ export function handler(req: Request): Response {
   const authUrl =
     `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&access_type=offline&prompt=consent`;
 
-  // Googleの認証ページにリダイレクト
   return Response.redirect(authUrl, 302);
 }
